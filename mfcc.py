d03c89f1f2e1c93f9505a3b64dd7dd1ec776c71f
SAURAV HIMMATRAO CHAVAN <61910452+SauravChavan@users.noreply.github.com>
2020-08-24 11:16:16 +0530

    Add required code

---
+++ mfcc.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 11:01:17 2020
+
+@author: SauravChavan
+"""
+
+def mel_spectrum(power_signal):
+    # add code here
+    
+def log_mel_spectrum(mel_spectrum_signal):
+    # add code
+    
+def mel_cepstrum(log_mel_spectrum_signal):
+    # add code
+    
+def 
---
+++ preprocessing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 10:34:38 2020
+
+@author: SauravChavan, VipulYadav, ShravaniPattewar, ShrutiSankpal
+"""
+def pre_empasize(signal):
+    # add code for pre-empasizing signal
+
+def windowing(pre_empasized_signal):
+    # add code for windowing signal
+    
+def power_spectrum(windowed_signal):
+    # add code for getting power spectrum of the signal
