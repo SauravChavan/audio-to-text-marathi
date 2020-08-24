b5558abb12a4249e4532f97448e4dc95f397b305
SAURAV HIMMATRAO CHAVAN <61910452+SauravChavan@users.noreply.github.com>
2020-08-24 15:45:59 +0530

    pre-emphasizing added

--- preprocessing.py
+++ preprocessing.py
@@ -4,8 +4,20 @@ Created on Mon Aug 24 10:34:38 2020
 
 @author: SauravChavan, VipulYadav, ShravaniPattewar, ShrutiSankpal
 """
-def pre_empasize(signal):
-    # add code for pre-empasizing signal
+import numpy as np
+
+def pre_empasize(signal, alpha=1, cof=0.98):
+    '''
+    In speech processing, the original signal usually has too much lower frequency energy, 
+    and processing the signal to emphasize higher frequency energy is necessary. 
+    To perform pre-emphasis, we choose some value α between .9 and 1. 
+    Then each value in the signal is re-evaluated using this formula: y[n] = x[n] - α*x[n-1]. 
+    This is apparently a first order high pass filter.
+    Source: https://math.stackexchange.com/
+            https://github.com/astorfi/speechpy/blob/master/speechpy/processing.py
+    '''
+    rolled_signal = np.roll(signal,shift)
+    return signal - alpha*rolled_signal
 
 def windowing(pre_empasized_signal):
     # add code for windowing signal
